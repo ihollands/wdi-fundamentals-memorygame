bb85152876e76b1380e09c4009ec96a7ad723a22
Ian Hollands <ihollands@outlook.com>
2016-08-31 09:28:35 -0400

    not really sure...practice?

--- Memorex/js/main.js
+++ Memorex/js/main.js
@@ -3,3 +3,28 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardOne === cardTwo) {
+	alert ("You found a match!")
+} else if (cardOne === cardThree) {
+	alert ("Sorry, try again.")
+} else if (cardOne === cardFour) {
+	alert ("Sorry, try again.")
+} else if (cardTwo === cardOne) {
+	alert ("You found a match!")
+} else if (cardTwo === cardThree) {
+	alert ("Sorry, try again.")
+} else if (cardTwo === cardFour) {
+	alert ("Sorry, try again.")
+} else if (cardThree === cardOne) {
+	alert ("Sorry, try again.")
+} else if (cardThree === cardTwo) {
+	alert ("Sorry, try again.")
+} else if (cardThree === cardFour) {
+	alert ("You found a match!")
+} else if (cardFour === cardOne) {
+	alert ("Sorry, try again.")
+} else if (cardFour === cardTwo) {
+	alert ("Sorry, try again.")
+} else if (cardFour === cardThree) {
+	alert ("You found a match!")
+}
