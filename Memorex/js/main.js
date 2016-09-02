d49fda14faa481b2f0f7b0805c7260e062d02a3a
Ian Hollands <ihollands@outlook.com>
2016-09-01 21:43:43 -0400

    added js to create game board and cards

--- Memorex/js/main.js
+++ Memorex/js/main.js
@@ -3,6 +3,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardOne === cardTwo) {
 	alert ("You found a match!")
 } else if (cardOne === cardThree) {
@@ -28,3 +29,15 @@ if (cardOne === cardTwo) {
 } else if (cardFour === cardThree) {
 	alert ("You found a match!")
 }
+*/
+
+var gameBoard = document.getElementById ('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < 4; i++) {
+		var newCardElement = document.createElement ('div');
+			newCardElement.className = 'card';
+			gameBoard.appendChild (newCardElement);
+	}
+}
+createBoard ();
